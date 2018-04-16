f98ea1c89bea8a847a3566f915f6ce9940e52e62
Mendess2526 <pedro.mendes.26@gmail.com>
2018-04-16 11:28:52 +0100

    Improves backupRunes

--- backupRunes.sh
+++ backupRunes.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+echo -e "\033[34mBack me up inside\033[0m"
 
 cd ~/gitProjects/spells
 
 mkdir -p runes
-cp ~/.vimrc runes/
-cp ~/.zshrc runes/
 
-git add runes/
+runeList=( ~/.vimrc ~/.zshrc )
+
+for rune in "${runeList[@]}";
+do
+    echo $rune
+    cp $rune runes/
+done
+
+git add runes/*
 git commit -m"Backup runes"
 git push
+
+if [[ $? == 1 ]];
+then
+    echo -e "\033[31mCan't backup\033[0m"
+else
+    echo -e "\033[32mBack me up and save me"
+fi
