a6d6ad3a36bfbe4d220f8d4924636588f836afed
Mendess2526 <pedro.mendes.26@gmail.com>
2018-10-13 01:56:32 +0100

    Backup runes | 13/10/18

--- backmeup.sh
+++ backmeup.sh
@@ -7,8 +7,10 @@ echo -e "\033[34mBack me up inside\033[0m"
 cd $(dirname "$(realpath $0)")
 
 git pull
+echo -e "\033[32m"
 git add -v runes/*
 git add -v $(basename $0).sh
+echo -e "\033[0m"
 git commit -m"Backup runes | "$(date +%d/%m/%y)
 git push
 
