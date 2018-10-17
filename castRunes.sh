d399b809c408e8db2055c1895a466b44a3d7b265
Mendess2526 <pedro.mendes.26@gmail.com>
2018-10-17 13:32:19 +0100

    Backup runes | 17/10/18

--- castRunes.sh
+++ castRunes.sh
@@ -6,6 +6,9 @@ cd $(dirname "$(realpath $0)")"/runes"
 
 for rune in "${runes[@]}";
 do
-    echo -e "\e[38;2;138;93;150mCasting "$(basename $rune)"\e[0m"
-    ln -fsv $(pwd)"/"$(basename $rune) $rune
+    if [[ $spell != $(basename $0) && ! -e ~/.local/bin/$(basename $spell .sh) ]]
+    then
+        echo -e "\e[38;2;138;93;150mCasting "$(basename $rune)"\e[0m"
+        ln -fsv $(pwd)"/"$(basename $rune) $rune
+    fi
 done
