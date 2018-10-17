47bbaf3c96a86e8323ce410ff18ddbf3ab8c9372
Mendess2526 <pedro.mendes.26@gmail.com>
2018-10-17 12:22:52 +0100

    Backup runes | 17/10/18

--- backmeup.sh
+++ backmeup.sh
@@ -4,7 +4,7 @@ echo -e "\033[34mBack me up inside\033[0m"
 
 cd $(dirname "$(realpath $0)")
 echo -e "\033[32m"
-git add -A
+git add --verbose --all
 echo -e "\033[0m"
 git commit -m"Backup runes | "$(date +%d/%m/%y)
 ./updatespellbook.sh
