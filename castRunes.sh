014b20e1d04b64798d4ee02923e9b9fdf61f52d9
Mendess2526 <pedro.mendes.26@gmail.com>
2018-10-17 13:45:36 +0100

    Backup spell book | 17/10/18

--- castRunes.sh
+++ castRunes.sh
@@ -6,7 +6,7 @@ cd $(dirname "$(realpath $0)")"/runes"
 
 for rune in "${runes[@]}";
 do
-    if [ -h $rune ]
+    if ! [ -h $rune ]
     then
         echo -e "\e[38;2;138;93;150mCasting "$(basename $rune)"\e[0m"
         ln -sv $(pwd)"/"$(basename $rune) $rune
