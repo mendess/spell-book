85517cff620cf8e130f830657d3aca3faf46dfdb
Mendess <pedro.mendes.26@gmail.com>
2018-10-17 12:19:36 +0100

    Make changeMeWall work on more desktop envs

--- changeMeWall.sh
+++ changeMeWall.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-image=$(ls -1 /home/mendess/Pictures/Wallpapers/ | shuf -n1)
-gsettings set org.gnome.desktop.background picture-uri file:///home/mendess/Pictures/Wallpapers/$image 2> ~/wallerror.log
-if (( $? != 0 ))
+
+if [[ $DESKTOP_SESSION == "i3" ]]
 then
-    echo -e "\033[31m"$image"\033[0m"
+    nitrogen --set-zoom-fill --random ~/Pictures/Wallpapers
 else
-    echo "Error: $image" 2> ~/wallerror.log
+    image=$(ls -1 /home/mendess/Pictures/Wallpapers/ | shuf -n1)
+    gsettings set org.gnome.desktop.background picture-uri file:///home/mendess/Pictures/Wallpapers/$image 2> ~/wallerror.log
+    if (( $? != 0 ))
+    then
+        echo -e "\033[31m"$image"\033[0m"
+    else
+        echo "Error: $image" 2> ~/wallerror.log
+    fi
 fi
