98b2c9687d8097456b177319bb79bb6ee012ebc7
Mendess2526 <pedro.mendes.26@gmail.com>
2018-10-19 18:02:58 +0100

    Backup spell book | 19/10/18

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if git --version
+if git --version &>/dev/null
 then
     git config --global user.name "Mendess2526"
     git config --global user.email "pedro.mendes.26@gmail.com"
     git config --global core.excludesfile '~/.gitignore'
+else
+    echo Please install git
 fi
