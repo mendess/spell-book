8134a86b0b6791279e02ba5605c151d0be2fb4a8
Mendess2526 <pedro.mendes.26@gmail.com>
2018-10-23 00:26:13 +0100

    Backup spell book | 23/10/18 00:26

--- learnSpells.sh
+++ learnSpells.sh
@@ -9,10 +9,10 @@ for spell in *.sh
 do
     if ! [ -e ~/.local/bin/$(basename $spell .sh) ]
     then
-        echo -e "\e[38;2;138;93;150mLearning "$(basename $spell)"\e[0m"
+        echo -e "\e[38;2;138;93;150mLearning "$(basename $spell .sh)"\e[0m"
         ln -sv $(pwd)"/"$spell ~/.local/bin/$(basename $spell .sh)
     else
-        echo -e "\033[36m"$spell"\033[32m already learned, skipping...\033[0m"
+        echo -e "\033[36m"$(basename $spell .sh)"\033[32m already learned, skipping...\033[0m"
     fi
 done
     
