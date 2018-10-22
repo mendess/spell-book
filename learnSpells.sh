6309d02025864748dc6eef553932887b0c0e4afc
Mendess2526 <pedro.mendes.26@gmail.com>
2018-10-23 00:24:07 +0100

    Backup spell book | 23/10/18 00:24

--- learnSpells.sh
+++ learnSpells.sh
@@ -7,7 +7,7 @@ cd $(dirname "$(realpath $0)")
 
 for spell in *.sh
 do
-    if [[ ! -e "~/.local/bin/$(basename $spell .sh)" ]]
+    if ! [ -e ~/.local/bin/$(basename $spell .sh) ]
     then
         echo -e "\e[38;2;138;93;150mLearning "$(basename $spell)"\e[0m"
         ln -sv $(pwd)"/"$spell ~/.local/bin/$(basename $spell .sh)
