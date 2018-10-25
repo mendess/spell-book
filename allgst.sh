870c080c019b758ca5bf1bdebe3e91f7bba889d2
Mendess2526 <pedro.mendes.26@gmail.com>
2018-10-25 21:52:34 +0100

    Backup spell book | 25/10/18 21:52

---
+++ allgst.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for dir in $(find . -mindepth 1 -maxdepth 1 -type d)
+do
+    cd $dir
+    if ! git diff-index --quiet HEAD --
+    then
+        echo -e "\033[32m$(pwd)\033[0m"
+        git status
+    fi
+    cd ..
+done
