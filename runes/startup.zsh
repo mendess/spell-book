a13025b3aab08f0ebbcfcbcd1d09f1a727c09ebc
Mendess2526 <pedro.mendes.26@gmail.com>
2018-11-18 17:18:04 +0000

    Backup spell book | 18/11/18 17:18

--- runes/startup.zsh
+++ runes/startup.zsh
@@ -1,7 +1,8 @@
 export RTV_EDITOR='nvim'
 export VISUAL='nvim'
+export EDITOR='nvim'
 export WWW_HOME='duckduckgo.com/lite'
 
 export PATH=~/.local/bin:$PATH
 
-fortune | cowthink $(echo " \n-b\n-d\n-g\n-p\n-s\n-t\n-w\n-y" | shuf -n1)
+#fortune | cowthink $(echo " \n-b\n-d\n-g\n-p\n-s\n-t\n-w\n-y" | shuf -n1)
