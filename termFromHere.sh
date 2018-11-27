48cdc0c095904331242052d04e2800a1120361c6
Mendess2526 <pedro.mendes.26@gmail.com>
2018-11-27 11:08:54 +0000

    Backup spell book | 27/11/18 11:08

--- termFromHere.sh
+++ termFromHere.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# depends: xorg-xdpyinfo xorg-xprop
 ID=$(xdpyinfo | grep focus | cut -f4 -d " ")
-PID=$(($(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3) + 2))
+PID=$(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3)
 if [ -e "/proc/$PID/cwd" ]
 then
     urxvt -cd $(readlink /proc/$PID/cwd) &
