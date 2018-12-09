1e7606e057f2ff73b61b19718b250bfd644c0b2f
Mendess2526 <pedro.mendes.26@gmail.com>
2018-12-09 19:04:44 +0000

    Backup spell book | 09/12/18 19:04

--- allgst.sh
+++ allgst.sh
@@ -2,7 +2,7 @@
 
 for dir in $(find . -mindepth 1 -maxdepth 1 -type d)
 do
-    cd $dir
+    cd "$dir" || continue
     if ! git diff-index --quiet HEAD --
     then
         echo -e "\033[32m$(pwd)\033[0m"
