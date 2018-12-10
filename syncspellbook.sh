488a8f4a038f293f5f700c4766cddb58e0c88687
Mendess2526 <pedro.mendes.26@gmail.com>
2018-12-10 00:42:11 +0000

    Backup spell book | 10/12/18 00:42

--- syncspellbook.sh
+++ syncspellbook.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 function sync {
-    echo -en "\033[32m"
-    git add --verbose --all
-    echo -en "\033[0m"
+    echo -en "\033[32m"; git add --verbose --all; echo -en "\033[0m"
 
-    if ! git diff-index --quiet HEAD --
-    then
+    if ! git diff-index --quiet HEAD --; then
+        hasCommits=1
         git commit -m"Backup spell book | $(date '+%d/%m/%y %H:%M')"
     else
+        hasCommits=0
         echo -e "\033[34mNothing to backup\033[0m"
     fi
 
@@ -18,35 +17,39 @@ function sync {
     fetch=$?
     echo -en "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
     echo -en "\033[0m"
-    if ! [ $fetch ]
-    then
+    if ! [ $fetch ]; then
         echo -e "\033[31mCan't access github\033[0m"
         return 2
     fi
 
-    if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]
-    then
-        git pull --rebase
+    if [ "$(git rev-parse HEAD)" != "$(git rev-parse @{u})" ]; then
+        hasPulls=1
+    else
+        echo -e "\033[34mNothing to pull\033[0m"
+        hasPulls=0
+    fi
 
-        while [[ $? != 0 ]]
+    if [ "$hasCommits" ] && [ "$hasPulls" ]; then
+        while git pull --rebase
         do
             echo -e "\033[31mConflicts emerged, please resolve them\033[0m"
-            read
+            read -r
             rebase=1
             for file in $(git status --short | grep UU | cut -d" " -f2)
             do
-                nvim $file || vim $file
+                nvim "$file" || vim "$file"
             done
             git add -A
             git rebase --continue
         done
 
-        if [[ $rebase != 0 ]]
-        then
+        if [[ $rebase != 0 ]]; then
             git push --quiet
         fi
-    else
-        echo -e "\033[34mNothing to pull\033[0m"
+    elif [ "$hasCommits" ]; then
+        git push --quiet
+    elif [ "$hasPulls" ]; then
+        git pull --quiet
     fi
 
     bash ./learnSpells.sh
@@ -54,7 +57,7 @@ function sync {
     return 0
 }
 
-cd $(dirname "$(realpath $0)")
+cd "$(dirname "$(realpath "$0")")" || exit 1
 echo -e "\033[34mBack me up inside\033[0m"
 sync
 if [ $? ]
