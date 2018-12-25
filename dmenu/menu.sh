699458a1cbf195e453aca34a4977ad132c9663b5
Mendess2526 <pedro.mendes.26@gmail.com>
2018-12-25 22:15:13 +0000

    Backup spell book | 25/12/18 22:15

--- dmenu/menu.sh
+++ dmenu/menu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname "$(realpath $0)")
 
-sh $(ls -l | tail -n +2 | awk '{print $9}' | grep -v 'menu' | dmenu -i -p "Pick a menu:" -l 5)
+sh $(ls -l | tail -n +2 | awk '{print $9}' | grep -v 'menu' | grep '\.sh' | dmenu -i -p "Pick a menu:" -l 5)
