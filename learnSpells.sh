b2f703d08c92aaf44a8f17aa2e923aa9cd19f866
Mendess2526 <pedro.mendes.26@gmail.com>
2018-12-29 00:57:49 +0000

    Backup spell book | 29/12/18 00:57

--- learnSpells.sh
+++ learnSpells.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function newSpells {
-    for spell in spells/*.sh
+    for spell in spells/*.spell
     do
-        [ -h ~/.local/bin/"$(basename "$spell" .sh)" ] || return 0
+        [ -h ~/.local/bin/"$(basename "$spell" .spell)" ] || return 0
     done
     return 1;
 }
@@ -14,12 +14,12 @@ newSpells || exit 0
 
 echo -e "\033[33mLearning Spells...\033[0m"
 
-for spell in spells/*.sh
+for spell in spells/*.spell
 do
-    if ! [ -e ~/.local/bin/"$(basename "$spell" .sh)" ]
+    if ! [ -e ~/.local/bin/"$(basename "$spell" .spell)" ]
     then
-        echo -e "\e[38;2;138;93;150m    $(basename "$spell" .sh)\e[0m"
-        ln -s "$(pwd)/$spell" ~/.local/bin/"$(basename "$spell" .sh)"
+        echo -e "\e[38;2;138;93;150m    $(basename "$spell" .spell)\e[0m"
+        ln -s "$(pwd)/$spell" ~/.local/bin/"$(basename "$spell" .spell)"
     fi
 done
 echo -e "\033[33mDone!\033[0m"
