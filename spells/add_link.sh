81aa973c972eb38d3cee79aefe27894c1e2026fe
Mendess2526 <pedro.mendes.26@gmail.com>
2018-12-30 11:43:22 +0000

    Backup spell book | 30/12/18 11:43

--- spells/add_link.sh
+++ spells/add_link.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$#" -lt 3 ]; then
-    echo "Usage: $0 <name> <link>"
+    echo "Usage: $0 <link>"
     exit 1
 fi
 
 cd "$(dirname "$(realpath "$0")")" || exit 1
-echo "$1;$2" >> ../cantrips/links
+echo "$(youtube-dl --get-title "$1");$1" >> ../cantrips/links
