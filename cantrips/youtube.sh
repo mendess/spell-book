3c0fb508f091e9a274d24b81d14b1717948ccb8f
Mendess2526 <pedro.mendes.26@gmail.com>
2019-04-07 01:49:04 +0100

    Backup spell book | 07/04/19 01:49

--- cantrips/youtube.sh
+++ cantrips/youtube.sh
@@ -1,31 +1,68 @@
 #!/bin/bash
 
+MODES="single
+shuff
+shuffA
+shuffC"
+
+mode=$(echo "$MODES" | dmenu -i -p "Mode?" -l "$(echo "$MODES" | wc -l)")
+
 vidlist=$(sed '/^$/ d' links)
 
-vidname="$(echo "$vidlist" | cut -d';' -f1 | dmenu -i -p "Which video? (type: \"shuff\" to pick at random)" -l "$(echo "$vidlist" | wc -l)")"
+case "$mode" in
+    single)
+        vidname="$(echo "$vidlist" \
+            | awk -F'\t' '{print $1}' \
+            | dmenu -i -p "Which video?" -l "$(echo "$vidlist" | wc -l)")"
 
-if [ "$vidname" = "shuff" ]; then
-    vid=$(echo "$vidlist" | shuf | sed '1q' | cut -d';' -f2)
-    title=$(echo "$vidlist" | grep "$vid" | cut -d';' -f1)
-elif [ "$vidname" = "shuffA" ]; then
-    tmp=$(echo "$vidlist" | shuf)
-    vid=$(echo "$tmp" | cut -d';' -f2 | xargs)
-    title="$(echo "$tmp" | cut -d';' -f1)"
-else
-    vid="$(echo "$vidlist" | grep -P "^$vidname;" | cut -d';' -f2)"
-    title="$vidname"
-fi
+        vid="$(echo "$vidlist" \
+            | grep -P "^$vidname" \
+            | awk -F'\t' '{print $2}')"
+
+        title="$vidname"
+        ;;
+    shuff)
+        vid=$(echo "$vidlist" \
+            | shuf \
+            | sed '1q' \
+            | awk -F'\t' '{print $2}')
+
+        title=$(echo "$vidlist" \
+            | grep "$vid" \
+            | awk -F'\t' '{print $1}')
+
+        ;;
+    shuffA)
+        tmp=$(echo "$vidlist" | shuf)
+        vid=$(echo "$tmp" | awk -F'\t' '{print $2}')
+        title="$(echo "$tmp" | awk -F'\t' '{print $1}')"
+        ;;
+    shuffC)
+        catg=$(echo "$vidlist" \
+            | awk -F'\t' '{for(i = 3; i <= NF; i++) { print $i } }' \
+            | tr '\t' '\n' \
+            | sed '/^$/ d' \
+            | sort \
+            | uniq \
+            | dmenu -i -p "Which category?")
+
+        vid="$(echo "$vidlist" | grep -P ".*\t.*\t.*$catg" | awk -F'\t' '{print $2}')"
+        title="$(echo "$vidlist" | grep -P ".*\t.*\t.*$catg" | awk -F'\t' '{print $1}')"
+        ;;
+esac
+
 [ "$vid" = "" ] && exit
 
 p=$(echo "no
 yes" | dmenu -i -p "With video?")
 
-if [ "$p" = "yes" ]
-then
-    mpv --input-ipc-server=/tmp/mpvsocket $vid
-elif [ "$p" = "no" ]
-then
-    cmd="echo -e '\n$title'; mpv --input-ipc-server=/tmp/mpvsocket --no-video $vid"
-    urxvt -fn "xft:DejaVu Sans Mono:autohint=true:size=20" -title 'my-media-player' -e bash -c "$cmd"
-fi
-
+case $p in
+    yes)
+        # shellcheck disable=SC2086
+        mpv --input-ipc-server=/tmp/mpvsocket $vid
+        ;;
+    no)
+        cmd="echo -e '\n$title'; mpv --input-ipc-server=/tmp/mpvsocket --no-video $vid"
+        urxvt -fn "xft:DejaVu Sans Mono:autohint=true:size=20" -title 'my-media-player' -e bash -c "$cmd"
+        ;;
+esac
