6e352ec657dde797fcccaa3244b86ddb13304194
Mendess2526 <pedro.mendes.26@gmail.com>
2019-04-12 21:00:22 +0100

    Backup spell book | 12/04/19 21:00

--- cantrips/youtube.sh
+++ cantrips/youtube.sh
@@ -46,7 +46,8 @@ case "$mode" in
             | uniq -c \
             | dmenu -i -p "Which category?" \
             | sed -E 's/^[ ]*[0-9]*[ ]*//')
-        vid="$(echo "$vidlist" | grep -P ".*\t.*\t.*$catg" | awk -F'\t' '{print $2}')"
+
+        vid="$(echo "$vidlist" | grep -P ".*\t.*\t.*$catg" | awk -F'\t' '{print $2}' | xargs)"
         title="$(echo "$vidlist" | grep -P ".*\t.*\t.*$catg" | awk -F'\t' '{print $1}')"
         ;;
 esac
