72ffbf5c49b305624dd4e6f3701f54a8dff96655
Mendess2526 <pedro.mendes.26@gmail.com>
2019-07-02 15:04:21 +0100

    Backup spell book | 02/07/19 15:04

--- cantrips/mconlinelist.sh
+++ cantrips/mconlinelist.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-MEMBERS=$(mcstatus "$(cat ~/.links/mcserverip)" status | tail -1 | sed -E -e 's/.*\[(.*)\]/\1/g' -e 's/, /\n/g' -e "s/'//g" | awk '{print $1}')
+MEMBERS=$(mcstatus "$(cat ~/.links/mcserverip)" status \
+    | grep players \
+    | cut -d' ' -f3- \
+    | sed -E -e 's/.*\[(.*)\]/\1/g' -e 's/, /\n/g' -e "s/'//g" \
+    | awk '$0 !~ /No players online/ {print $1}')
+
 if [ -z "$MEMBERS" ]
 then
     notify-send "No one online"
