9195a2e03550eed029d753eec3b2cb9f4b07540c
Mendess2526 <pedro.mendes.26@gmail.com>
2019-09-12 06:21:10 +0100

    Backup spell book | 12/09/19 06:21

--- scrolls/basicWF.sh
+++ scrolls/basicWF.sh
@@ -1,102 +1,20 @@
 #!/bin/bash
 
+echo "$#"
+exit
+
 script_dir="$(dirname "$(realpath "$0")")"
 
-packages=(
-acpi
-alsa-utils
-bc
-clang
-cmake
-compton
-cronie
-curl
-dash
-dmenu
-dunst
-entr
-feh
-firefox
-gdb
-gimp
-gparted
-htop
-i3-gaps
-i3lock
-i3status
-imagemagick
-jq
-mpv
-neofetch
-network-manager-applet
-nitrogen
-nmap
-noto-fonts-emoji
-openssh
-parted
-pkgconf
-pulseaudio
-pulseaudio-alsa
-pulseaudio-bluetooth
-pygmentize
-python-pip
-python-pygments
-rsync
-rtv
-rustup
-rxvt-unicode
-scrot
-shellcheck
-socat
-surf
-sxiv
-tmux
-tree
-ttf-dejavu
-unzip
-urxvt-perls
-wget
-xclip
-xorg-server
-xorg-xinit
-xorg-xrandr
-xorg-xdpyinfo
-xz
-youtube-dl
-zathura
-zip
-zsh
-)
-
-aurpackages=(
-dropbox
-dropbox-cli
-discord
-entr
-fortune-mod
-ncpamixer
-pacmixer
-toilet
-urxvt-resize-font-git
-shellcheck-static
-)
-
-cargopackages=(
-tealdeer
-hyperfine
-exa
-bat
-cargo-watch
-racer
-)
-
-bloat=(
-nano
-)
+packages=()
+aurpackages=()
+bloat=()
+cargopackages=()
+eval "$(cat "$script_dir"/packages.sh)"
 
 sudo pacman -S  "${packages[@]}"
 
 sudo pacman -Rsn "${bloat[@]}"
+exit
 
 ## NEOVIM
 pip3 install --user pynvim
@@ -106,8 +24,6 @@ nvim -c PlugInstall -c qall
 
 #Oh My Zsh
 curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-
-# Rust
 
 # Yay (AUR manager)
 cd /tmp || exit 1
