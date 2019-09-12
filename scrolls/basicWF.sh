cbb4ead3aa04f8b0e96a02c89d84747797960411
Mendess2526 <pedro.mendes.26@gmail.com>
2019-09-12 07:18:09 +0100

    Backup spell book | 12/09/19 07:18

--- scrolls/basicWF.sh
+++ scrolls/basicWF.sh
@@ -1,7 +1,48 @@
 #!/bin/bash
 
-echo "$#"
-exit
+if [ "$#" = 0 ]
+then
+    ALL=1
+else
+    while [[ $# -gt 0 ]]
+    do
+    key="$1"
+
+    case $key in
+        -p|--pacman)
+        PACMAN=1
+        shift
+        ;;
+        -a|--aur)
+        AUR=1
+        shift
+        ;;
+        -z|--zsh)
+        ZSH=1
+        shift
+        ;;
+        -c|--cargo)
+        CARGO=1
+        shift
+    esac
+    done
+fi
+
+function pac {
+    test "$ALL" || "$PACMAN"
+}
+
+function aur {
+    test "$ALL" || "$AUR"
+}
+
+function zsh {
+    test "$ALL" || "$ZSH"
+}
+
+function carg {
+    test "$ALL" || "$CARGO"
+}
 
 script_dir="$(dirname "$(realpath "$0")")"
 
@@ -11,38 +52,44 @@ bloat=()
 cargopackages=()
 eval "$(cat "$script_dir"/packages.sh)"
 
-sudo pacman -S  "${packages[@]}"
+pac && yes n | sudo pacman -S  "${packages[@]}"
 
-sudo pacman -Rsn "${bloat[@]}"
-exit
+pac && yes n | sudo pacman -Rsn "${bloat[@]}"
 
 ## NEOVIM
-pip3 install --user pynvim
-pip3 install --user --upgrade pynvim
+pac && pip3 install --user pynvim
+pac && pip3 install --user --upgrade pynvim
 
-nvim -c PlugInstall -c qall
+pac && nvim -c PlugInstall -c qall
 
 #Oh My Zsh
-curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+zsh && \
+    curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh \
+    | sh
 
 # Yay (AUR manager)
+aur && {
 cd /tmp || exit 1
 git clone https://aur.archlinux.org/yay.git
 cd yay || exit 1
 makepkg -s
 
 yay -S "${aurpackages[@]}"
+}
 
-cargo install "${cargopackages[@]}"
+car && cargo install "${cargopackages[@]}"
 
 # Compton
-sudo pacman -S libconfig libxdg-basedir asciidoc
+pac && {
+yes n | sudo pacman -S libconfig libxdg-basedir asciidoc
+exit
 cd /tmp || exit 1
 git clone https://github.com/tryone144/compton
 cd compton || exit 1
 make
 make docs
 sudo make install
+}
 
 cd "$script_dir" || exit 1
 ../spells/syncspellbook.spell
