c5ad1963fd2fc2b59d92425a3ab3ff1a5ad5e301
Mendess2526 <pedro.mendes.26@gmail.com>
2019-11-10 17:13:07 +0000

    Backup spell book | 10/11/19 17:13

--- runes/bash/compleations.bash
+++ runes/bash/compleations.bash
@@ -23,13 +23,13 @@ complete -F _f svim
 
 _ssh()
 {
-    local cur prev opts
+    local cur opts
+    # local prev
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    #prev="${COMP_WORDS[COMP_CWORD-1]}"
     opts=$(grep '^Host' ~/.ssh/config ~/.ssh/config.d/* 2>/dev/null | grep -v '[?*]' | cut -d ' ' -f 2-)
-
-    COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
+    mapfile -t COMPREPLY < <(compgen -W "$opts" -- "$cur")
     return 0
 }
 
