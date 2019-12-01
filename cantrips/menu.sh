9a08cd6d9e618d4a2310dadcf867ee2e9fc8f4d4
Mendess2526 <pedro.mendes.26@gmail.com>
2019-12-01 17:05:06 +0000

    Backup spell book | weatherlight@01/12/19 17:05

--- cantrips/menu.sh
+++ cantrips/menu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$(realpath "$0")")" || exit 1
 
-./"$(find . | grep -v 'menu' | grep '\.sh' | sed -e 's|./||g' -e 's/\.sh$//g' | sort -r | dmenu -i -p "Pick a menu:" -l 5)".sh &
+./"$(find . | grep -v 'menu' | grep '\.sh' | sed -e 's|./||g' -e 's/\.sh$//g' | sort -r | dmenu -i -p "Pick a menu:" -l 100)".sh &
 diswon
