636ea0e7b234bd6ab9ead8bbdd4432784958ba0c
Mendess2526 <pedro.mendes.26@gmail.com>
2019-12-25 23:53:33 +0000

    Backup spell book | weatherlight@25/12/19 23:53

--- cantrips/menu.sh
+++ cantrips/menu.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "$(realpath "$0")")" || exit 1
 
 ./"$(find . | grep -v 'menu' | grep '\.sh' | sed -e 's|./||g' -e 's/\.sh$//g' | sort -r | dmenu -i -p "Pick a menu:" -l 100)".sh &
-diswon
+disown
---
+++ scrolls/theme.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget https://github.com/Mrcuve0/Aritim-Dark/archive/master.zip
+unzip master.zip
+mkdir -p ~/.themes
+mv Aritim-Dark-master/GTK ~/.themes/ayu
