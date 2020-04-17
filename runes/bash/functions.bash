6e91a3bf48a1770ef5ad32d6d9ec08020230b1fe
mendess <pedro.mendes.26@gmail.com>
2020-04-17 11:26:10 +0100

    Backup spell book | tolaria@17/04/20 11:26

--- runes/bash/functions.bash
+++ runes/bash/functions.bash
@@ -10,8 +10,8 @@ aura() {
             # pacman "$1" "$2"
             ;;
         -Ss)
-            curl -s "https://aur.archlinux.org/rpc/?v=5&type=search&by=name&arg=$1" \
-                | jq '.results[] | "\(.Name) -> \(.Description)"'
+            curl -s "https://aur.archlinux.org/rpc/?v=5&type=search&by=name&arg=$1" |
+                jq '.results[] | "\(.Name) -> \(.Description)"'
             ;;
         -S)
             aura "${@:2}"
@@ -28,12 +28,10 @@ aura() {
 }
 
 make() {
-    if [ -e Makefile ] || [ -e makefile ]
-    then
+    if [ -e Makefile ] || [ -e makefile ]; then
         bash -c "make $*"
     else
-        for i in *.c;
-        do
+        for i in *.c; do
             file=${i//\.c/}
             bash -c "make $file"
         done
@@ -41,31 +39,31 @@ make() {
 }
 
 ex() {
-  if [ -f "$1" ] ; then
-    case "$1" in
-      *.tar.bz2)   tar xjf "$1"   ;;
-      *.tar.gz)    tar xzf "$1"   ;;
-      *.bz2)       bunzip2 -v "$1"   ;;
-      *.rar)       unrar x "$1"   ;;
-      *.gz)        gunzip "$1"    ;;
-      *.tar)       tar xf "$1"    ;;
-      *.tbz2)      tar xjf "$1"   ;;
-      *.tgz)       tar xzf "$1"   ;;
-      *.zip)       unzip "$1"     ;;
-      *.Z)         uncompress "$1";;
-      *.7z)        7z x "$1"      ;;
-      *.xz)        xz -d "$1"     ;;
-      *)           echo "$1 cannot be extracted via ex()" ;;
-    esac
-  else
-    echo "$1 is not a valid file"
-  fi
+    if [ -f "$1" ]; then
+        case "$1" in
+            *.tar.bz2) tar xjf "$1" ;;
+            *.tar.gz) tar xzf "$1" ;;
+            *.bz2) bunzip2 -v "$1" ;;
+            *.rar) unrar x "$1" ;;
+            *.gz) gunzip "$1" ;;
+            *.tar) tar xf "$1" ;;
+            *.tbz2) tar xjf "$1" ;;
+            *.tgz) tar xzf "$1" ;;
+            *.zip) unzip "$1" ;;
+            *.Z) uncompress "$1" ;;
+            *.7z) 7z x "$1" ;;
+            *.xz) xz -d "$1" ;;
+            *) echo "$1 cannot be extracted via ex()" ;;
+        esac
+    else
+        echo "$1 is not a valid file"
+    fi
 }
 
 __append_to_recents() { # $1 line, $2 recents file
     mkdir -p ~/.cache/my_recents
     touch ~/.cache/my_recents/"$2"
-    echo "$1" | cat - ~/.cache/my_recents/"$2" | awk '!seen[$0]++' | head -10 > temp && mv temp ~/.cache/my_recents/"$2"
+    echo "$1" | cat - ~/.cache/my_recents/"$2" | awk '!seen[$0]++' | head -10 >temp && mv temp ~/.cache/my_recents/"$2"
 }
 
 __run_disown() {
@@ -76,7 +74,7 @@ __run_disown() {
         file=$HOME${file//\~//}
     fi
     file="$(realpath "$file")"
-    "$1" "$file" &> /dev/null &
+    "$1" "$file" &>/dev/null &
     disown
     [ -e "$file" ] && __append_to_recents "$file" "$1"
 }
@@ -106,18 +104,16 @@ alarm() {
 matrix() {
     echo -e "\e[1;40m"
     clear
-    while :
-    do
-        echo $LINES $COLUMNS $(( RANDOM % COLUMNS)) $(( RANDOM % 72 ))
+    while :; do
+        echo $LINES $COLUMNS $((RANDOM % COLUMNS)) $((RANDOM % 72))
         sleep 0.05
-    done \
-        | awk '{ letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()"; c=$4; letter=substr(letters,c,1);a[$3]=0;for (x in a) {o=a[x];a[x]=a[x]+1; printf "\033[%s;%sH\033[2;32m%s",o,x,letter; printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,letter;if (a[x] >= $1) { a[x]=0; } }}'
+    done |
+        awk '{ letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()"; c=$4; letter=substr(letters,c,1);a[$3]=0;for (x in a) {o=a[x];a[x]=a[x]+1; printf "\033[%s;%sH\033[2;32m%s",o,x,letter; printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,letter;if (a[x] >= $1) { a[x]=0; } }}'
 }
 
 loop() {
     n=""
-    while ! [ "$n" = "n" ]
-    do
+    while ! [ "$n" = "n" ]; do
         "$@"
         read -r n
     done
@@ -144,10 +140,9 @@ discordStream() {
     echo https://www.discordapp.com/channels/"$1"/"$2" | xclip -sel clip
 }
 
-
 svim() {
     if [ -n "$1" ]; then
-	    "$EDITOR" "$SPELLS"/"$1"
+        "$EDITOR" "$SPELLS"/"$1"
     else
         cd "$SPELLS" || exit 1
         local DIR
@@ -158,8 +153,7 @@ svim() {
 }
 
 k() {
-	if lsusb | grep 'Mechanical' | grep 'Keyboard' &>/dev/null
-    then
+    if lsusb | grep 'Mechanical' | grep 'Keyboard' &>/dev/null; then
         setxkbmap us
         xmodmap -e 'keycode  21 = plus equal plus equal'
     else
@@ -168,7 +162,10 @@ k() {
 }
 
 advent-of-code() {
-    if [[ "$1" != day* ]]; then echo "bad input: '$1'"; exit 1; fi
+    if [[ "$1" != day* ]]; then
+        echo "bad input: '$1'"
+        exit 1
+    fi
     cargo new "$1" || exit 1
     cd "$1" || exit 1
     echo '
@@ -178,7 +175,7 @@ path = "src/one.rs"
 
 [[bin]]
 name = "two"
-path = "src/two.rs"' >> Cargo.toml
+path = "src/two.rs"' >>Cargo.toml
     cp src/main.rs src/one.rs
     cp src/main.rs src/two.rs
     rm src/main.rs
@@ -208,8 +205,7 @@ record() {
 
 share() {
     FILE="$*"
-    if [ -d "$FILE" ]
-    then
+    if [ -d "$FILE" ]; then
         zip -r "/tmp/$(basename "$FILE").zip" "$FILE"
         FILE="/tmp/$(basename "$FILE").zip"
     fi
@@ -228,11 +224,21 @@ connect() {
 }
 
 gcl() {
-    if [[ "$1" == */* ]]; then
-        git clone git@github.com:"$1" "${@:2}"
-    else
-        git clone git@github.com:"$(git config --global user.name)"/"$1" "${@:2}"
-    fi
+    case "$1" in
+        http://*)
+            echo '============================> using http'
+            git clone "$1" "${@:2}"
+            ;;
+        git@* | https://*)
+            git clone "$1" "${@:2}"
+            ;;
+        */*)
+            git clone git@github.com:"$1" "${@:2}"
+            ;;
+        *)
+            git clone git@github.com:"$(git config --global user.name)"/"$1" "${@:2}"
+            ;;
+    esac
 }
 
 nospace() {
@@ -241,6 +247,12 @@ nospace() {
     done
 }
 
-which () {
+which() {
     declare -f | command which --read-functions "$@"
+}
+
+xdofast() {
+    alias x='xdotool'
+    alias xk='xdotool key'
+    alias xt='xdotool type'
 }
