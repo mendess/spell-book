987336ce6f56395e5afbfe666678f555a9880b6d
mendess <pedro.mendes.26@gmail.com>
2020-06-20 14:46:52 +0100

    Backup spell book | tolaria@20/06/20 14:46

--- cantrips/game.sh
+++ cantrips/game.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Launch steam games from the /comfy/ terminal with dmenu
+# Launch steam games from /comfy/ dmenu
 
 games="$(grep -n "name" ~/.local/share/Steam/steamapps/*.acf ~/.hdd/SteamLibrary/steamapps/*acf \
     | sed -E 's/.*appmanifest_([0-9]+)\.acf.*"name".*"([^"]+)"/\1\t\2/g')"
@@ -17,3 +17,4 @@ name="$(echo "$games" | sed -r 's/[0-9]+\s//' | grep -v -i -E 'proton|redistribu
 
 [ -n "$name" ] || exit
 echo "$games" | grep "$name" | awk -F'\t' '{print $1}' | xargs -I{} steam 'steam://run/{}'
+killall steam
