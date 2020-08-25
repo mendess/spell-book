ff0841f08865d4e7711429cf985dc2b92b2c1949
mendess <pedro.mendes.26@gmail.com>
2020-08-25 11:33:46 +0100

    Backup spell book | tolaria@25/08/20 11:33

--- runes/m/update_panel.sh
+++ runes/m/update_panel.sh
@@ -1,3 +1,34 @@
 #!/bin/sh
+%% switch on hostname
+%% localhost {
+CURRENT="$(m current \
+    | head -2 \
+    | tail -1 \
+    | cut -d ':' -f2 \
+    | sed '
+    s/　/  /g;
+    s/Ａ/A /g;
+    s/Ｃ/C /g;
+    s/Ｈ/H /g;
+    s/Ｉ/I /g;
+    s/Ｌ/L /g;
+    s/Ｍ/M /g;
+    s/Ｎ/N /g;
+    s/Ｔ/T /g;
+    s/Ｕ/U /g;' |
+    sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+
+termux-notification --type media \
+    --alert-once \
+    --id 1 \
+    --title "$CURRENT" \
+    --media-next "echo playlist-next | socat - '$SOCKET'"\
+    --media-pause "echo cycle pause | socat - '$SOCKET'"\
+    --media-play "echo cycle pause | socat - '$SOCKET'"\
+    --media-previous "echo playlist-prev | socat - '$SOCKET'"
+%% }
+%% default {
 pkill -10 -x lemon
+%% }
+%% end
 :
