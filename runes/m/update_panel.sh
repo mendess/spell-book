d15f9a8782c93f71093a78055e67acc300deadea
mendess <pedro.mendes.26@gmail.com>
2020-09-07 10:06:30 +0100

    Backup spell book | localhost@07/09/20 10:06

--- runes/m/update_panel.sh
+++ runes/m/update_panel.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 case "$(hostname)" in
     localhost)
+        m="$(m c | awk 'NR > 1 && NR < 4 {print} {last=$0} END {print last}')"
+        title="$(echo "$m" | sed 2q)"
+        content="$(echo "$m" | tail -1)"
         termux-notification \
-            --title "$(m current --short)" \
-            --content "Up next: $(m c | tail -1)" \
+            --title "$title" \
+            --content "Up next: $content" \
             --type media \
             --alert-once \
             --id 1 \
-            --on-delete "echo cycle pause | socat - ~/.cache/mpvsocket_cache"\
+            --on-delete "echo '{ \"command\": [\"set_property\", \"pause\", true] }' |\
+                socat - ~/.cache/mpvsocket_cache" \
             --media-next "echo playlist-next | socat - ~/.cache/mpvsocket_cache"\
             --media-pause "echo cycle pause | socat - ~/.cache/mpvsocket_cache"\
             --media-play "echo cycle pause | socat - ~/.cache/mpvsocket_cache"\
@@ -18,4 +22,3 @@ case "$(hostname)" in
         pkill -10 -x lemon
         ;;
 esac
-:
