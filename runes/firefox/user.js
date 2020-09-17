6491e42c3f0a1cc541b3c7eb7764fb89cf0f7c3e
mendess <pedro.mendes.26@gmail.com>
2020-09-17 19:20:32 +0100

    Backup spell book | weatherlight@17/09/20 19:20

--- runes/firefox/user.js
+++ runes/firefox/user.js
@@ -2,3 +2,4 @@ user_pref("browser.fullscreen.autohide", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
 user_pref("ui.context_menus.after_mouseup", true);
+user_pref("gfx.webrender.all", true);
