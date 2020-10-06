751055be0add2c889ba560371b1878d9cda57ccd
mendess <pedro.mendes.26@gmail.com>
2020-10-06 23:04:16 +0100

    Backup spell book | localhost@06/10/20 23:04

--- runes/bash/startup.bash
+++ runes/bash/startup.bash
@@ -2,37 +2,10 @@
 
 [ -n "$SSH_CONNECTION" ] && [ -e /dev/stderr ] && exec >> /dev/stderr && export TERM=xterm-256color
 
-start_tmux() {
-    echo -n "Launch tmux session? [Y/n] "
-    read -r r
-    if [[ $r == "" ]] || [[ $r == "y" ]] || [[ $r == "Y" ]]; then
-        if pgrep tmux &> /dev/null;
-        then
-            echo -e "\033[1mWhich session?\033[0m"
-            tmux list-sessions \
-                -F '#{session_name}:	(#{t:session_created})	#{?session_attached,yes,no}' \
-                | column -s'	' -t -N 'name,session created at,atached'
-
-            echo -n "(Nothing for a new session)> "
-            read -r s
-            if [ -z "$s" ]
-            then
-                tmux -2
-            elif ! tmux attach -t "$s"
-            then
-                echo "Starting a new session"
-                tmux -2
-            fi
-        else
-            tmux -2
-        fi
-        exit
-    fi
-}
-
 if [[ "$(hostname)" =~ tolaria|mirrodin|weatherlight|matess ]] &&
     [[ -z "$TMUX" ]] && [ -n "$SSH_CONNECTION" ] && hash tmux 2>/dev/null; then
-    start_tmux
+    tmux -2 new -A -s default
+    exit
 fi
 
 if [[ -n $TMUX ]]; then
