32c088d8b63b4560b93d10a7e8bc7fa611c44db1
mendess <pedro.mendes.26@gmail.com>
2020-10-10 23:05:09 +0100

    Backup spell book | matess@10/10/20 23:05

--- runes/bash/startup.bash
+++ runes/bash/startup.bash
@@ -2,19 +2,17 @@
 
 [ -n "$SSH_CONNECTION" ] && [ -e /dev/stderr ] && exec >>/dev/stderr && export TERM=xterm-256color
 
-if [[ "$(hostname)" =~ tolaria|mirrodin|weatherlight|matess ]] &&
-    [[ -z "$TMUX" ]] && [ -n "$SSH_CONNECTION" ] && command -v tmux &>/dev/null; then
-    tmux -2 new -A -s default
-    exit
-fi
-
 if [[ -n $TMUX ]]; then
     exit() {
         tmux detach
     }
 fi
 
-if [[ -e /tmp/todo ]]; then
-    cat /tmp/todo
-fi
-rsync mirrodin:todo /tmp/ & disown
+[[ -z "$TMUX" ]] && [[ "$SSH_CONNECTION" ]] && command -v tmux &>/dev/null &&
+    tmux -2 new -A -s default &&
+    exit
+
+TMPDIR="${TMPDIR:-/tmp}"
+[[ -e "$TMPDIR/todo" ]] && cat "$TMPDIR/todo"
+rsync mirrodin:todo "$TMPDIR" &
+disown
