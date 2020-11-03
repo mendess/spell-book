02e571b1dbad759e1e10e5e932c136dbe31119a3
mendess <pedro.mendes.26@gmail.com>
2020-11-03 17:29:23 +0000

    Backup spell book | tolaria@03/11/20 17:29

--- runes/bash/startup.bash
+++ runes/bash/startup.bash
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-TMPDIR="${TMPDIR:-/tmp}"
-[[ -e "$TMPDIR/todo" ]] &&
-    echo -e "\e[1;31m" &&
-    LC_ALL=C sort "$TMPDIR/todo" &&
-    echo -e "\e[0m"
-rsync mirrodin:todo "$TMPDIR" &>/dev/null &
-disown
+echo -e "\e[1;31m"
+todo --list --bg-pull
+echo -e "\e[0m"
