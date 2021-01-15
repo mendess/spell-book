c740c7d48cb99657db11d6bfb18f5e6dc2429d4b
mendess <pedro.mendes.26@gmail.com>
2021-01-15 00:36:26 +0000

    Backup spell book | tolaria@15/01/21 00:36

---
+++ scrolls/nfs_walls.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo pacman -S --needed --noconfirm nfs-utils
+
+if ! showmount -e mirrodin; then
+    echo "please fix"
+fi
+
+mkdir -p ~/.local/share/wallpapers
+sudo mount mirrodin:/Wallpapers .local/share/wallpapers
+cat <<EOF | tee -a /etc/fstab
+# /nfs/Wallpapers
+mirrodin:/Wallpapers /home/mendess/.local/share/wallpapers nfs defaults,timeo=900,retrans=5,_netdev 0 0
+EOF
