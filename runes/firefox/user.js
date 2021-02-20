66e98f858fdfae1756afcef76374b1cefbb94a71
mendess <pedro.mendes.26@gmail.com>
2021-02-20 15:46:11 +0000

    Backup spell book | kaladesh@20/02/21 15:46

--- runes/firefox/user.js
+++ runes/firefox/user.js
@@ -3,4 +3,5 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
 user_pref("ui.context_menus.after_mouseup", true);
 user_pref("gfx.webrender.all", true);
+user_pref("extensions.webextensions.restrictedDomains", "");
 user_pref("browser.ssb.enabled", true);
