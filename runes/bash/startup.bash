ad184a4618e7841e75a3a4bbc12174ccbf180639
mendess <pedro.mendes.26@gmail.com>
2021-05-12 14:32:19 +0100

    kaladesh => [startup.bash][check_repos.spell]

--- runes/bash/startup.bash
+++ runes/bash/startup.bash
@@ -4,3 +4,12 @@ echo -en "\e[1;31m"
 command -V todo &>/dev/null &&
     todo --list --bg-pull
 echo -en "\e[0m"
+
+{
+    cat /tmp/memo
+    {
+        nmcli -t --fields NAME connection show --active | grep -q 'ZON.*2010' &&
+            scp -q mirrodin:memo /tmp/memo
+    } &
+    disown
+} 2>/dev/null
