77c3e01c8ae6319e6366d2d7c3cd994ef1d07001
mendess <pedro.mendes.26@gmail.com>
2021-06-11 13:06:16 +0100

    kaladesh => [startup.bash]

--- runes/bash/startup.bash
+++ runes/bash/startup.bash
@@ -7,6 +7,9 @@ echo -en "\e[0m"
 
 command -V nmcli &>/dev/null && {
     nmcli -t --fields NAME connection show --active | grep -q 'ZON.*2010' &&
-        { cat /tmp/memo 2>/dev/null || : ; } &&
-        scp -q mirrodin:memo /tmp/memo
+        { cat /tmp/memo 2>/dev/null || :; }
+    (
+        scp -q mirrodin:memo /tmp/memo &
+
+    )
 }
