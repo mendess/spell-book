025c4ef29fb2620e4ee230184b8e13d9e4267b86
mendess <pedro.mendes.26@gmail.com>
2021-10-22 16:08:01 +0100

    kaladesh => [3 bash][7 compleations][1 library][1 runes]

--- runes/bash/completions.bash
+++ runes/bash/completions.bash
@@ -155,7 +155,7 @@ completions_dir=/usr/share/bash-completion/completions/
 command -V youtube-dl &>/dev/null &&
     eval "$(sed 's/youtube-dl/ytdl/' < <(complete -p youtube-dl 2>/dev/null))"
 
-for c in "$SPELLS"/runes/bash/compleations/*; do
+for c in "$SPELLS"/runes/bash/completions/*; do
     #shellcheck disable=1090
     . "$c"
 done
