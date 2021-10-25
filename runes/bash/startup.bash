ab8f238c43eb9512e0e5bcb0a58f0a6962ad7244
mendess <pedro.mendes.26@gmail.com>
2021-10-25 22:55:41 +0100

    tolaria => [profile][startup.bash][todo.spell]

--- runes/bash/startup.bash
+++ runes/bash/startup.bash
@@ -4,12 +4,3 @@ echo -en "\e[1;3$((RANDOM % 7 + 1))m"
 command -V todo &>/dev/null &&
     todo --list --bg-pull
 echo -en "\e[0m"
-
-command -V nmcli &>/dev/null && {
-    nmcli -t --fields NAME connection show --active | grep -q 'ZON.*2010' &&
-        { cat /tmp/memo 2>/dev/null || :; }
-    (
-        scp -q mirrodin:memo /tmp/memo &
-
-    )
-}
