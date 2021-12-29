54d33b07e216d1f8137b77f53a82b3e11afa379f
mendess <pedro.mendes.26@gmail.com>
2021-12-29 22:50:25 +0000

    tolaria => [youtube.sh][profile][temp_shortcuts.bash]

--- cantrips/youtube.sh
+++ cantrips/youtube.sh
@@ -2,7 +2,7 @@
 # Launches the music player controled using [m](./spells/m.spell)
 
 set -x
-m gui play-interactive
+SESSION_KIND=gui m play-interactive
 command -v jukebox && { pgrep jukebox ||
     [[ "$(pgrep youtube | wc -l)" -gt 2 ]] ||
     while :; do
--- runes/bash/temp_shortcuts.bash
+++ runes/bash/temp_shortcuts.bash
@@ -2,3 +2,10 @@
 
 alias purge-podspec='gco multiplatform-sdk/src/iosMain/resources/SpeechifySDK.podspec'
 alias check-apps='gradle build; ./scripts/check-example-apps.sh'
+
+mac-check() {
+    exec 3<>/dev/tcp/new-phyrexia/8912
+    echo "$(git rev-parse --abbrev-ref HEAD)" >&3
+    read -r -u 3
+    echo "$REPLY"
+}
