a3370adf385f4ec26de24f00b5ad3cc3a340f1be
mendess <pedro.mendes.26@gmail.com>
2022-05-02 10:41:39 +0100

    tolaria => [stolenMagic.sh]

--- stolenMagic.sh
+++ stolenMagic.sh
@@ -16,15 +16,12 @@ install-script() {
         cd ~/.local/bin/
         wget --quiet -N "$1" |& grep -v ^SSL_INIT$
         chmod +x "$(basename "$1")"
-        echo "downloaded: $(user-from-link <<<"$1")/$(basename "$1")"
+        echo "  - $(basename "$1")"
     )
 }
 
-printf "\n\033[33mStealing magic from:\033[0m\n"
-printf "%s\n" "${scripts[@]}" | user-from-link | sort -u | while read -r user; do
-    echo -e "  -> $user"
-done
-echo
+printf "\n\033[33mStealing magic from \033[1m%s\033[0;33m users\033[0m\n" \
+    $(printf "%s\n" "${scripts[@]}" | user-from-link | sort -u | wc -l)
 
 for s in "${scripts[@]}"; do
     install-script "$s" &
