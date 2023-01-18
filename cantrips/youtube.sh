39b944a5ba9ec59437803b1a78bbe424f147c0c4
mendess <pedro.mendes.26@gmail.com>
2023-01-18 21:25:12 +0000

    tolaria => [youtube.sh][profile][re.sh][synclinks.sh]

--- cantrips/youtube.sh
+++ cantrips/youtube.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 # Launches the music player controled using [m](./spells/m.spell)
 
-set -x
 RUST_LOG=debug SESSION_KIND=gui m play-interactive
-command -v jukebox && { pgrep jukebox ||
-    [[ "$(pgrep youtube | wc -l)" -gt 2 ]] ||
-    while :; do
-        jukebox --room "$(hostname)" jukebox </dev/null
-        [[ ! "$i" ]] && i=0
-        sleep "$((i++))"m
-    done
-}
