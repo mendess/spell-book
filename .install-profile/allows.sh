b1e15d4b88092952968c3b6c4fad5de1d3029428
mendess <pedro.mendes.26@gmail.com>
2023-01-19 01:13:14 +0000

    tolaria => [allows.sh][syncspellbook.spell]

---
+++ .install-profile/allows.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+profiles_folder=$(dirname -- "$0")
+
+script="$1"
+want_to_install="$2"
+
+! [ -e "$profiles_folder/$script.profile" ] ||
+    {
+        while read -r pattern; do
+            grep -qE "$pattern" <<<"$want_to_install" && exit 0
+        done < "$profiles_folder/$script.profile"
+        exit 1
+    }
