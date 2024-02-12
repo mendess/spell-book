7a994b5a9a2fd5ba612e31cc1ee75eb7a36cce5d
mendess <pedro.mendes.26@gmail.com>
2024-02-12 01:06:15 +0000

    tolaria => [alacritty.toml][profile][start-river.sh]

--- runes/river/start-river.sh
+++ runes/river/start-river.sh
@@ -2,6 +2,11 @@
 
 export MOZ_ENABLE_WAYLAND=1
 export XKB_DEFAULT_OPTIONS=caps:escape
+export LIBVA_DRIVER_NAME=nvidia
+export XDG_SESSION_TYPE=wayland
+export GBM_BACKEND=nvidia-drm
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
+export WLR_NO_HARDWARE_CURSORS=1
 
 # TODO: change this to exec
 river -c "$HOME/.config/river/init first-boot"
