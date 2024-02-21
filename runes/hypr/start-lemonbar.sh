18b7d5e990bc07c4afb20334f160bbbc2015248a
mendess <pedro.mendes.26@gmail.com>
2024-02-21 18:14:16 +0000

    tolaria => [start-lemonbar.sh][swhkdrc]

--- runes/hypr/start-lemonbar.sh
+++ runes/hypr/start-lemonbar.sh
@@ -2,4 +2,4 @@
 
 mapfile -t outputs < <(hyprctl monitors -j | jq 'map("--output " + .name) | .[]' -r | tr '[:space:]' '\n')
 pkill -x lemon
-RUST_LOG=${RUST_LOG:-debug} lemon --program zelbar "${outputs[@]}" "$@" &>/tmp/$(whoami)/lemon.log
+RUST_LOG=${RUST_LOG:-debug} lemon --program zelbar "${outputs[@]}" "$@" &>"/tmp/$(whoami)/lemon.log"
