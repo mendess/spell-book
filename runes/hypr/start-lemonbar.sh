e247a9d37f1295b804b4842a9ddaeee41122295b
mendess <pedro.mendes.26@gmail.com>
2024-11-23 13:48:43 +0000

    weatherlight => [start-lemonbar.sh]

--- runes/hypr/start-lemonbar.sh
+++ runes/hypr/start-lemonbar.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-mapfile -t outputs < <(hyprctl monitors -j | jq 'map("--output " + .name) | .[]' -r | tr '[:space:]' '\n')
-pkill -x lemon
-RUST_LOG=${RUST_LOG:-debug} lemon --program zelbar "${outputs[@]}" "$@" &>"/tmp/$(whoami)/lemon.log"
+while :; do
+    mapfile -t outputs < <(hyprctl monitors -j | jq 'map("--output " + .name) | .[]' -r | tr '[:space:]' '\n')
+    pkill -x lemon
+    RUST_LOG=${RUST_LOG:-debug} lemon --program zelbar "${outputs[@]}" "$@" &>"/tmp/$(whoami)/lemon.log"
+done
