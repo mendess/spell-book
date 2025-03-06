c67c1bf9f3bfd549068be184a7fbe0524401e49c
mendess <pedro.mendes.26@gmail.com>
2025-03-06 19:07:20 +0000

    weatherlight => [start-swhkd.sh]

--- runes/hypr/start-swhkd.sh
+++ runes/hypr/start-swhkd.sh
@@ -3,4 +3,11 @@
 exec &>/tmp/mendess/swhkd.log
 
 swhks &
-pkexec swhkd --cooldown 400 --config ~/.config/swhkd/swhkdrc
+pkexec swhkd --cooldown 400 --config ~/.config/swhkd/swhkdrc &
+while :; do
+    if rfkill | grep -q ' blocked'; then
+        rfkill unblock bluetooth wlan
+        break
+    fi
+done
+wait
