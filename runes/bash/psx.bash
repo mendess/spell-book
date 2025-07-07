f32b856890c28bd808dcf6ff95be4883980bde37
mendess <pedro.mendes.26@gmail.com>
2025-07-07 18:15:09 +0100

    weatherlight => [ps1.crs][psx.bash]

--- runes/bash/psx.bash
+++ runes/bash/psx.bash
@@ -4,25 +4,6 @@ __truncPath() {
     pwd | sed -r -e 's!^'"$HOME"'!~!g' -e 's|([^/])[^/]+/|\1/|g' | tr -d '\n'
     return "$1"
 }
-__exit_code() {
-    # 148 is the sig stop code, so I can Ctrl+Z without getting an error
-    [ "$1" -gt 0 ] && [ "$1" -ne 148 ] && printf "<%s>" "$1"
-}
-
-__git_branch() {
-    if [[ -e .git ]] ||
-        [[ -e ../.git ]] ||
-        [[ -e ../../.git ]] ||
-        [[ -e ../../../.git ]] ||
-        [[ -e ../../../../.git ]]; then
-        if [ -n "$SSH_CLIENT" ]; then
-            printf '::'
-        fi
-        git symbolic-ref HEAD --short 2>/dev/null |
-            sed -r -e 's|([^/][^/]?)[^/]+/|\1/|g' -e 's/^(.{10}).*/\1+/'
-    fi
-    return "$1"
-}
 
 __c() {
     local NO_COLOUR="\e[00m"
@@ -32,24 +13,8 @@ __c() {
         "$PRINTING_OFF" "$1" "$PRINTING_ON" "$2" "$PRINTING_OFF" "$NO_COLOUR" "$PRINTING_ON"
 }
 
-__has_n_job() {
-    if [[ "$(jobs | grep -c 'Stopped')" -eq "$1" ]]; then
-        printf ::
-    fi
-    return "$2"
-}
-
-__has_more_than_job() {
-    if [[ "$(jobs | grep -c 'Stopped')" -gt $1 ]]; then
-        printf ::
-    fi
-    return "$2"
-}
-
 __no_new_line_fix() {
   local _ y x _
-  local LIGHT_YELLOW="\001\033[1;93m\002"
-  local     RESET="\001\e[0m\002"
 
   IFS='[;' read -p $'\e[6n' -d R -rs _ y x _
   if [[ "$x" != 1 ]]; then
@@ -65,7 +30,7 @@ __move_cursor_to_start_of_ps1() {
     else
         command=$(history 1 | sed 's/^\s*[0-9]*\s*//')
         command_length=${#command}
-        ps1_prompt_length=${#PS1_PROMPT}
+        ps1_prompt_length=2 # '> '
         ((total_length = command_length + ps1_prompt_length + 1))
         ((lines = total_length / COLUMNS))
         ((vertical_movement = lines + 1))
@@ -73,54 +38,15 @@ __move_cursor_to_start_of_ps1() {
     tput cuu $vertical_movement
 }
 
-BOLD_RED="\e[1;31m"
-BOLD_GREEN="\e[1;32m"
-BOLD_YELLOW="\e[1;33m"
-BOLD_BLUE="\e[1;34m"
-BOLD_MAGENTA="\e[1;35m"
-BOLD_CYAN="\e[1;36m"
-BOLD_GRAY="\e[1;2m"
-RED="\e[0;31m"
-GREEN="\e[0;32m"
 YELLOW="\e[0;33m"
 BLUE="\e[0;34m"
-MAGENTA="\e[0;35m"
 
-PS1_PROMPT="> "
-PS2_PROMPT="| "
-PS4_PROMPT="\$0.\$LINENO+ "
 RESTORE_CURSOR_POSITION="\e[u"
 SAVE_CURSOR_POSITION="\e[s"
 TIMESTAMP="\A"
-BATTERY="\$(cat /sys/class/power_supply/BAT0/capacity)% "
-case "$(hostname)" in
-    tolaria)
-        hostname_color=$BOLD_BLUE
-        ;;
-    weatherlight | kaladesh)
-        hostname_color=$BOLD_CYAN
-        ;;
-    mirrodin)
-        hostname_color=$BOLD_GRAY
-        ;;
-    argentum)
-        hostname_color=$BOLD_YELLOW
-        ;;
-    *)
-        hostname_color=$BOLD_GREEN
-        ;;
-esac
 
-SSH_PROMPT="$(__c "$BOLD_RED" '\u')@$(__c "$hostname_color" '\h')"
-G_BRANCH='$(__git_branch $?)'
 T_PATH='$(__truncPath $?)'
-EXIT_STATUS='$(__exit_code $?)'
-NO_JOB='$(__has_n_job 0 $?)'
-ONE_JOB=$(__c "$MAGENTA" '$(__has_n_job 1 $?)')
-TWO_JOB=$(__c "$BLUE" '$(__has_n_job 2 $?)')
-LOTS_JOB=$(__c "$RED" '$(__has_more_than_job 2 $?)')
 NO_NEW_LINE_FIX='$(__no_new_line_fix $?)'
-JOBS=("$NO_JOB" "$ONE_JOB" "$TWO_JOB" "$LOTS_JOB")
 
 PS0_ELEMENTS=(
     "$SAVE_CURSOR_POSITION" "\$(__move_cursor_to_start_of_ps1)"
@@ -133,23 +59,8 @@ PS0=$(
     echo "${PS0_ELEMENTS[*]}"
 )
 
-PS1_ELEMENTS=("$NO_NEW_LINE_FIX")
-if { [[ "$(tty)" == *tty* ]] || [ "$TTY_TMUX" ]; } && [ -f /sys/class/power_supply/BAT0/capacity ]; then
-    PS1_ELEMENTS+=("$BATTERY")
-fi
-if [[ -n "$SSH_CLIENT" ]]; then
-    PS1_ELEMENTS+=("$SSH_PROMPT")
-fi
-PS1_ELEMENTS+=(
-    "$G_BRANCH" "${JOBS[@]}" '<'
-    "$(__c "$YELLOW" "$T_PATH")" "$(__c "$BOLD_RED" "$EXIT_STATUS")"
-    '> '
-)
-PS1=$(
-    IFS=
-    echo "${PS1_ELEMENTS[*]}"
-)
+PS1='$(__no_new_line_fix $?)$($SPELLS/runes/bash/helpers/ps1.crs $? "$(jobs)")'
 
-PS2="$(__c "$BLUE" "$PS2_PROMPT")"
+PS2="$(__c "$BLUE" "| ")"
 
-PS4="$(__c "$BLUE" "$PS4_PROMPT")"
+PS4="$(__c "$BLUE" "\$0:\$LINENO: ")"
