8f202c98e5a8b541957c603a50edeef6ffe8d5c2
mendess <pedro.mendes.26@gmail.com>
2026-04-21 00:55:41 +0100

    weatherlight => [session-management.sh]

--- runes/hypr/session-management.sh
+++ runes/hypr/session-management.sh
@@ -15,14 +15,14 @@ opt=$(printf 'lock\nsuspend\nreboot\nshutdown\n' | picker -nb '#6b0213' -sb '#a8
 case "$opt" in
     lock)
         CACHE=${XDG_CACHE_HOME:-~/.cache}/changeMeWall
-        c1=$(cat "${CACHE}/wall_colors" | sed -n 1p | cut -d' ' -f1 | sed 's/#//')
-        c1_t=$(cat "${CACHE}/wall_colors" | sed -n 1p | cut -d' ' -f2 | sed 's/#//')
-        c2=$(cat "${CACHE}/wall_colors" | sed -n 2p | cut -d' ' -f1 | sed 's/#//')
-        c3=$(cat "${CACHE}/wall_colors" | sed -n 3p | cut -d' ' -f1 | sed 's/#//')
+        c1=$(sed -n 1p <"${CACHE}/wall_colors"  | cut -d' ' -f1 | sed 's/#//')
+        c1_t=$(sed -n 1p <"${CACHE}/wall_colors"  | cut -d' ' -f2 | sed 's/#//')
+        c2=$(sed -n 2p <"${CACHE}/wall_colors"  | cut -d' ' -f1 | sed 's/#//')
+        c3=$(sed -n 3p <"${CACHE}/wall_colors"  | cut -d' ' -f1 | sed 's/#//')
         dunstctl set-paused true
         SESSION_KIND=cli m set-pause > /dev/null
-        swaylock --inside-clear-color="$c1" --text-clear-color="$c1_t" --ring-clear-color="$c3"
-                 --inside-ver-color="$c1"   --text-ver-color="$c1_t"   --ring-ver-color="$c3"
+        swaylock --inside-clear-color="$c1" --text-clear-color="$c1_t" --ring-clear-color="$c3" \
+                 --inside-ver-color="$c1"   --text-ver-color="$c1_t"   --ring-ver-color="$c3" \
                  --ring-color="$c3" --key-hl-color="$c2"
         dunstctl set-paused false
     ;;
