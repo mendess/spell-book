8386a287a46a4e15ab4015c066d29bf9cc78241b
pmendes <pedro.mendes.26@gmail.com>
2026-04-20 14:42:03 +0100

    oboro => [start-swhkd.sh]

--- runes/hypr/start-swhkd.sh
+++ runes/hypr/start-swhkd.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-exec &>/tmp/mendess/swhkd.log
+log=/tmp/$(whoami)/swhkd.log
+mkdir -p "$(dirname "$log")"
+exec &>"$log"
 
 swhks &
 swhkd --cooldown 400 --log ${XDG_RUNTIME_DIR:-$HOME/.local/share}/swhks-current_unix_time.log
