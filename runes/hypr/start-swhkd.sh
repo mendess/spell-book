c2253652fb97498b2076580e82aa629bca272ab3
mendess <pedro.mendes.26@gmail.com>
2026-04-21 00:46:23 +0100

    weatherlight => [start-swhkd.sh]

--- runes/hypr/start-swhkd.sh
+++ runes/hypr/start-swhkd.sh
@@ -5,4 +5,6 @@ mkdir -p "$(dirname "$log")"
 exec &>"$log"
 
 swhks &
-swhkd --cooldown 400 --log ${XDG_RUNTIME_DIR:-$HOME/.local/share}/swhks-current_unix_time.log
+swhkd \
+    --cooldown 400 \
+    --log "${XDG_RUNTIME_DIR:-$HOME/.local/share}/swhks-current_unix_time.log"
